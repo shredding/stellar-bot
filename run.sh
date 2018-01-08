14e3e3ae94293ee30eabff350acc81f02ebc8cf0
Christian Peters <chp@digitale-avantgarde.com>
2018-01-08 14:04:45 +0100

    restart policy

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ git pull
 docker build -t stellar-bot .
 
 docker stop app || true && docker rm app || true
-docker run --link=db:db -itd --name app stellar-bot:latest
+docker run --restart=always --link=db:db -itd --name app stellar-bot:latest
